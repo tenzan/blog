6d293fba55438559af7bc5111f72f03467bb6485
Askar Karasaev <askar.karasaev@gmail.com>
2016-04-17 19:51:46 +0900

    Add the ability to view a list of all posts

---
+++ spec/factories/post_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :post do
+    title 'Sample post'
+    body 'Sample content'
+  end
+end
---
+++ spec/features/viewing_posts_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature 'Users can view posts' do
+  scenario 'with post content' do
+    post = FactoryGirl.create(:post,
+       title: 'Test post',
+       body: 'Content for test post')
+
+    visit '/'
+    click_link 'Test post'
+    expect(page.current_url).to eq post_url(post)
+  end
+end
