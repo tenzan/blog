6335d83662dec63eb0950eca7e3c340ae7f23348
Askar Karasaev <askar.karasaev@gmail.com>
2016-04-17 16:47:29 +0900

    Users can create posts feature added

---
+++ spec/features/creating_posts_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature 'Users can create new posts' do
+  scenario 'with valid attributes' do
+    visit '/'
+    click_link 'New Post'
+    fill_in 'Title', with: 'Sublime Text 3'
+    fill_in 'Body', with: 'A text editor for everyone'
+    click_button 'Create Post'
+    expect(page).to have_content 'Post has been created.'
+  end
+end
