2c13d228a64c1787c20643f5fa2e8b0ce3d34cce
Askar Karasaev <askar.karasaev@gmail.com>
2016-04-17 18:34:23 +0900

    Controller and model added for Post

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -4,9 +4,11 @@ RSpec.feature 'Users can create new posts' do
   scenario 'with valid attributes' do
     visit '/'
     click_link 'New Post'
+
     fill_in 'Title', with: 'Sublime Text 3'
     fill_in 'Body', with: 'A text editor for everyone'
-    click_button 'Create Post'
+
+    click_button 'Create post'
     expect(page).to have_content 'Post has been created.'
   end
 end
